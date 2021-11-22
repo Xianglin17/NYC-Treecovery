4e4cb3029efcc3382a2d4297fd952868902ed4e8
Xianglin Chen <xianglin.chen@baruchmail.cuny.edu>
2021-11-22 20:21:33 +0000

    complaint_fact_table

--- models/fact_tables/Complaint_Fact.sql
+++ models/fact_tables/Complaint_Fact.sql
@@ -1,22 +1,22 @@
-with complaint as (
+with complaint_table as (
     select * from {{ref('Complaint_Dimension')}}
 ),
-agency as (
+agency_table as (
     select * from {{ref('Agency_Dimension')}} 
 ),
-datetable as (
+datetable_table as (
     select *
     from {{ref('Date_Dimension')}} 
 ),
-location as (
+location_table as (
     select *
     from {{ref('Location_Dimension')}} 
 ),
-total as (
+total_table as (
     select Created_Year as Year, 
             Created_Month as Month, 
             Created_Day as Day,
-            Date_ID,
+            Created_Date as Date_ID,
             latitude as Latitude,
             longitude as Longtitude,
             city as City,
@@ -30,9 +30,11 @@ total as (
             from {{ref('311_complaints')}} 
 )
 
-select Complaint_Type_Dim_ID,Agency_Dim_ID,Date_Dim_ID,Location_Dim_ID
-from Total 
-LEFT JOIN complaintUSING (complaint_type,status)
-LEFT JOIN agency USING (agency,agency_name)
-LEFT JOIN datetable USING (Year, Month, Day, Date_ID)
-LEFT JOIN location USING (Latitude, Longtitude, City, ZipCode,Borough, STATE)
+select Complaint_Type_Dim_ID, Agency_Dim_ID, Date_Dim_ID,Location_Dim_ID, count(*) as Number_of_Complaint
+from total_table 
+LEFT JOIN complaint_table USING (complaint_type,status)
+LEFT JOIN agency_table USING (agency,agency_name)
+LEFT JOIN datetable_table USING (Year, Month, Day, Date_ID)
+LEFT JOIN location_table USING (Latitude,Longtitude,City,ZipCode,Borough,STATE)
+Group by Complaint_Type_Dim_ID, Agency_Dim_ID, Date_Dim_ID,Location_Dim_ID
+order by Number_of_Complaint DESC
