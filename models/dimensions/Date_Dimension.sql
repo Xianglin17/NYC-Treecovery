b167b5908fdd80f4e85c0053b3c076e57b9a53a2
Xianglin Chen <xianglin.chen@baruchmail.cuny.edu>
2021-11-22 19:05:38 +0000

    fact_table_building

--- models/dimensions/Date_Dimension.sql
+++ models/dimensions/Date_Dimension.sql
@@ -2,5 +2,5 @@ with all_date as (select * from {{ref('stg_complaints_date')}}
 Union ALL
 select * from {{ref('stg_weather_date')}})
 
-select DISTINCT *,ROW_NUMBER() OVER (ORDER BY (SELECT NULL)) AS location_dim_ID
+select DISTINCT *,ROW_NUMBER() OVER (ORDER BY (SELECT NULL)) AS Date_dim_ID
 from all_date
