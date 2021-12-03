dda00662a105b87d73a1f3856661f58c88d02ea9
Xianglin Chen <xianglin.chen@baruchmail.cuny.edu>
2021-12-03 15:05:43 +0000

    Correction

--- models/fact_tables/Complaint_Fact.sql
+++ models/fact_tables/Complaint_Fact.sql
@@ -16,23 +16,22 @@ location_table as (
     from {{ref('Location_Dimension')}} 
 ),
 closed_date_table as (
-    select  Date_Dim_ID as Closed_Date_Dim_ID,
+    select  ROW_NUMBER() OVER (ORDER BY (SELECT NULL)) as Closed_Date_Dim_ID,
             Date_ID as Closed_Date  
-    from(select      
+    from(select distinct    
             Closed_Year as Year, 
             Closed_Month as Month, 
             Closed_Day as Day,
             Closed_Date as Date_ID 
           from {{ref('311_complaints')}}
           ) 
-    Left Join 
-        (select * from {{ref('Date_Dimension')}} ) USING (Year, Month, Day, Date_ID)
 ),
 total_table as (
     select Created_Year as Year, 
             Created_Month as Month, 
             Created_Day as Day,
             Created_Date as Date_ID,
+            Closed_Date, 
             latitude as Latitude,
             longitude as Longtitude,
             city as City,
@@ -44,13 +43,14 @@ total_table as (
             complaint_type,
             status
             from {{ref('311_complaints')}} 
+            where Created_Year > 2016
 )
 SELECT Complaint_Type_Dim_ID, 
        Agency_Dim_ID, 
        Created_Date_Dim_ID,
        Closed_Date_Dim_ID,
        Location_Dim_ID,
-       count(*) as Number_of_Complaint,
+       count(Final_ID) as Number_of_Complaint,
        ABS(date_diff(DATE(Closed_Date),DATE(Date_ID),day)) as Date_Gap
 
 FROM (
@@ -59,16 +59,17 @@ SELECT Complaint_Type_Dim_ID,
        Date_Dim_ID as Created_Date_Dim_ID,
        Closed_Date_Dim_ID,
        Location_Dim_ID,
+       ROW_NUMBER() OVER (ORDER BY (SELECT NULL)) as Final_ID,
        Closed_Date,
        Date_ID
 
 
-from (total_table 
-LEFT JOIN complaint_table USING (complaint_type,status)
-LEFT JOIN agency_table USING (agency,agency_name)
-LEFT JOIN date_table USING (Year, Month, Day, Date_ID)
-LEFT JOIN location_table USING (Latitude,Longtitude,City,ZipCode,Borough,STATE)),
-closed_date_table
+from total_table 
+INNER JOIN complaint_table USING (complaint_type,status)
+INNER JOIN agency_table USING (agency,agency_name)
+INNER JOIN date_table USING (Year, Month, Day, Date_ID)
+INNER JOIN location_table USING (Latitude,Longtitude,City,ZipCode,Borough,STATE)
+INNER JOIN closed_date_table USING (Closed_Date)
 )
 
 Group by Complaint_Type_Dim_ID, Agency_Dim_ID, Created_Date_Dim_ID, Location_Dim_ID, Closed_Date_Dim_ID, Closed_Date, Date_ID
