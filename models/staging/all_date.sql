9c5738e0b218d3fc94926a8acb1a4d3d3c8d74d2
Xianglin Chen <xianglin.chen@baruchmail.cuny.edu>
2021-12-03 16:05:51 +0000

    corrections

--- models/staging/all_date.sql
+++ models/staging/all_date.sql
@@ -6,7 +6,7 @@ WITH DATE_SPINE as (
 {{ dbt_utils.date_spine(
     datepart="day",
     start_date="cast('2010-01-01' as date)",
-    end_date="cast('2021-01-01' as date)"
+    end_date="cast('2022-01-01' as date)"
    )
 }} )
 
